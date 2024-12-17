bba7e46b601a45c31a94740a3cf00ab8bf0033a3
infobarbosa <infobarbosa@gmail.com>
2024-12-17 10:47:28 -0300

    adicionando script clean-up.sh para remover arquivos das pastas ./data e ./datasets

---
+++ clean-up.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Remove files from ./data directory
+rm -Rf ./data/* ./data/.*
+
+# Remove files from ./datasets directory
+rm -Rf ./datasets/* ./datasets/.*
+
+echo "Arquivos das pastas ./data e ./datasets foram removidos."
