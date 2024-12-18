1353196dd2802a539e42bd6942617fb32a0fed5e
infobarbosa <infobarbosa@gmail.com>
2024-12-18 18:28:55 -0300

    adicionando esquema para leitura da tabela clientes em formato CSV

--- scripts/clientes.py
+++ scripts/clientes.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, LongType, StringType, DateType
 
 # Inicializa a sessão Spark
 spark = SparkSession.builder \
@@ -12,9 +13,19 @@ spark._jsc.hadoopConfiguration().set("fs.s3a.endpoint", "http://minio:9000")
 spark._jsc.hadoopConfiguration().set("fs.s3a.path.style.access", "true")
 spark._jsc.hadoopConfiguration().set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
 
-# Lê a tabela clientes em formato CSV
+# Define o esquema para o CSV
+schema = StructType([
+    StructField("ID", LongType(), True),
+    StructField("NOME", StringType(), True),
+    StructField("DATA_NASC", DateType(), True),
+    StructField("CPF", StringType(), True),
+    StructField("EMAIL", StringType(), True)
+])
+
+# Lê a tabela clientes em formato CSV com o esquema definido
 clientes_df = spark.read \
     .format("csv") \
+    .schema(schema) \
     .option("header", "true") \
     .option("delimiter", ";") \
     .option("compression", "gzip") \
