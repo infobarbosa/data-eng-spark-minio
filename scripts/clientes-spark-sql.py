0ebb3288bb611137f72fd46e9f1e0920b55381ed
infobarbosa <infobarbosa@gmail.com>
2024-12-18 15:40:41 -0300

    adicionando configurações do MinIO na inicialização da sessão Spark

--- scripts/clientes-spark-sql.py
+++ scripts/clientes-spark-sql.py
@@ -1,8 +1,13 @@
 from pyspark.sql import SparkSession
 
-# Inicializa a sessão Spark
+# Inicializa a sessão Spark com configurações para acessar o MinIO
 spark = SparkSession.builder \
     .appName("Clientes Spark SQL") \
+    .config("spark.hadoop.fs.s3a.access.key", "minioadmin") \
+    .config("spark.hadoop.fs.s3a.secret.key", "minioadmin") \
+    .config("spark.hadoop.fs.s3a.endpoint", "http://minio:9000") \
+    .config("spark.hadoop.fs.s3a.path.style.access", "true") \
+    .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
     .getOrCreate()
 
 # Consulta SQL para obter os dados da tabela ecommerce.clientes
